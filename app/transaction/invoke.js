06be0e025e90f837ec22ec5000fb3cadb8870335
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-01-31 08:48:24 -0300

    Update invoke and query for new contract

--- app/transaction/invoke.js
+++ app/transaction/invoke.js
@@ -99,6 +99,51 @@ module.exports = {
             console.error(`Failed to submit transaction: ${error}`);
             return 3;
         }
+    },
+
+    async saveProArticle(transactionId, userSender, userReciver, document, requisition, rede) {
+        console.log(rede)
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'networks', rede.nomeRede, 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+            let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+            
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.resolve(__dirname, '..', '..', 'networks', rede.nomeRede, 'chaincode','wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+
+            // Check to see if we've already enrolled the user.
+            const identity = await wallet.get(userPki);
+            if (!identity) {
+                console.log(`An identity for the user "${userPki}" does not exist in the wallet`);
+                return 2;
+            }
+
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: userPki, discovery: { enabled: true, asLocalhost: true } });
+
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork(rede.nomeCanal);
+
+            // Get the contract from the network.
+            const contract = network.getContract('fastenetwork', 'ProArticle');
+
+            // Submit the specified transaction.
+            await contract.submitTransaction("createTransactionProArticle", transactionId, userSender, userReciver, document, requisition);
+            console.log('Transaction has been submitted');
+
+            // Disconnect from the gateway.
+            await gateway.disconnect();
+
+            return 1;
+
+        } catch (error) {
+            console.error(`Failed to submit transaction: ${error}`);
+            return 3;
+        }
     }
 
 }
